21fe2b2567b044d10ae8379fac4d143bf2d3aac4
Shevok <egorshevko@gmail.com>
2019-03-18 01:15:34 +0300

    add Potato class

---
+++ object-oriented-programming/src/main/java/com/netcracker/courses/oop/Vegetables/Potato.java
@@ -0,0 +1,10 @@
+package com.netcracker.courses.oop.Vegetables;
+
+import com.netcracker.courses.oop.typeofvegetable.Vegetable;
+
+public class Potato extends Vegetable {
+
+    public Potato(int weight) {
+        super("Картошка", "roots", weight, 43);
+    }
+}
