5741e79b060a1ed56875dd3cf86e600e2f26cc44
Shevok <egorshevko@gmail.com>
2019-03-18 14:03:39 +0300

    add Potato class

---
+++ object-oriented-programming/src/main/java/com/netcracker/courses/oop/tubercrops/Potato.java
@@ -0,0 +1,11 @@
+package com.netcracker.courses.oop.tubercrops;
+
+import com.netcracker.courses.oop.typeofvegetable.TuberCrops;
+
+public class Potato extends TuberCrops {
+
+    public Potato(int weight) {
+        super("Картошка", "Tuber crops", weight, 43);
+    }
+
+}
