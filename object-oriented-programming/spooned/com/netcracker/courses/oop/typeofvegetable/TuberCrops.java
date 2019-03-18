d6fc01b945c12c96e9eb563a7964f2ea07cf0816
Shevok <egorshevko@gmail.com>
2019-03-18 14:06:20 +0300

    add TuberCrops class

---
+++ object-oriented-programming/spooned/com/netcracker/courses/oop/typeofvegetable/TuberCrops.java
@@ -0,0 +1,10 @@
+package com.netcracker.courses.oop.typeofvegetable;
+
+
+// клубнеплоды (картофель, батат, топинамбур)
+public class TuberCrops extends Vegetable {
+    public TuberCrops(java.lang.String name, java.lang.String category, int weight, int calories) {
+        super(name, category, weight, calories);
+    }
+}
+
