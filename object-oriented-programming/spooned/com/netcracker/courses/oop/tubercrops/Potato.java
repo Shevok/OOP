6c815256521e2f1b76180bca2861efef41eaefd3
Shevok <egorshevko@gmail.com>
2019-03-27 22:52:35 +0300

    delete spooned

--- object-oriented-programming/spooned/com/netcracker/courses/oop/tubercrops/Potato.java
+++
@@ -1,9 +0,0 @@
-package com.netcracker.courses.oop.vegetables.roots.tubercrops;
-
-
-public class Potato extends com.netcracker.courses.oop.vegetables.roots.typeofvegetable.TuberCrops {
-    public Potato(int weight) {
-        super("Картошка", "Tuber crops", weight, 43);
-    }
-}
-
